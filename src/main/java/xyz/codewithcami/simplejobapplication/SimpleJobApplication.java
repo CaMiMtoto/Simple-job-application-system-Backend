ada68547b8e82f05afc7a6359cf91f2e91905e7d
CaMiMtoto <jeanpaulcami@live.com>
2022-03-21 18:44:37 +0200

    change allowed domain to https://elegant-elion-18eadc.netlify.app/

--- src/main/java/xyz/codewithcami/simplejobapplication/SimpleJobApplication.java
+++ src/main/java/xyz/codewithcami/simplejobapplication/SimpleJobApplication.java
@@ -24,7 +24,7 @@ public class SimpleJobApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://guarded-ocean-90978.herokuapp.com")
+                        .allowedOrigins("http://localhost:3000", "https://elegant-elion-18eadc.netlify.app")
                         .allowedMethods("PUT", "DELETE", "GET", "POST")
                         .allowCredentials(true)
                         .maxAge(3600);
